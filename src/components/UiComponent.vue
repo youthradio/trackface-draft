38606cec51cc16118f8efffa8be741aaab4a6912
shagia <usenet@shagia.me>
2020-03-09 16:48:04 -0700

    make and edit icon styling

---
+++ src/components/UiComponent.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="ui-container">
+        <ul class="tool-list">
+            <li><a class="tool-icon" href="#"><img :src="require('../../public/icons/pencil.png')" /> </a></li>
+            <li><a class="tool-icon" href="#"><img :src="require('../../public/icons/strokebrush.png')" /> </a></li>
+            <li><a class="tool-icon" href="#"><img :src="require('../../public/icons/paintbrush.png')" /> </a></li>
+            <li><a class="tool-icon" href="#"><img :src="require('../../public/icons/sticker.png')" /> </a></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MainUI',
+    props: {
+  }
+}
+</script>
+<style lang="scss" scoped>
+.tool-list{
+    display: inline-flex;
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    background-color: #C4C4C4;
+    width: 100%;
+    justify-content: center;
+    .tool-icon {
+        width: 100px;
+        height: 100px;
+        text-align: center;
+        line-height: 100px;
+        vertical-align: middle;
+        padding: 30px;
+        border: 1px solid white;
+        border-radius: 100%;
+    }
+    
+    img {
+        vertical-align: middle;
+    }
+}
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'es6-promise/auto'
 
 Vue.config.productionTip = false
 
