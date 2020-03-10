ea87c78592e48f9865929da3d7eac88833845e40
shagia <usenet@shagia.me>
2020-03-09 17:27:21 -0700

    add canvas element

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <MainUI />
+    <Canvas />
   </div>
 </template>
 
 <script>
 import MainUI from './components/UiComponent.vue'
+import Canvas from './components/Canvas.vue'
 
 export default {
   name: 'App',
   components: {
-    MainUI
+    MainUI,
+    Canvas
   }
 }
 </script>
