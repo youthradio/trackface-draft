6d187180610b348bca2ed31ad262d069c09e4d58
shagia <usenet@shagia.me>
2020-03-09 18:43:46 -0700

    change icon button spacing

--- src/components/UiComponent.vue
+++ src/components/UiComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="ui-container">
     <ul class="tool-list">
-      <li v-for="tool in toolsData" :key="tool.name">
+      <li class="tool-list-item" v-for="tool in toolsData" :key="tool.name">
         <a href="#"
           ><img class="tool-icon" :src="require(`../assets/${tool.image}`)" />
         </a>
@@ -31,7 +31,7 @@ export default {
   margin: 0;
   padding: 0;
   .tool-icon {
-    max-width: 100px;
+    max-width: 100%;
     height: auto;
     text-align: center;
     border: 5px solid $gray;
@@ -39,6 +39,9 @@ export default {
     position: relative;
     overflow: hidden;
     border-radius: 50%;
+  }
+  .tool-list-item{
+      margin: 0.5rem;
   }
 }
 </style>
