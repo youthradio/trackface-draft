4bd8f5a3493904423cf9e992d8312f2a8a13fa9f
shagia <usenet@shagia.me>
2020-03-24 15:09:43 -0700

    give enable / disable style conditional to the color picker tool

--- src/components/UiComponent.vue
+++ src/components/UiComponent.vue
@@ -12,7 +12,7 @@
       </div>
       <div class="tool-icon-container">
         <div
-          :class="[toolState.strokeSizeMenu ? 'colors' : 'hidden']"
+          :class="[toolState.strokeSizeMenu ? 'enabled' : 'hidden']"
           class="strokes"
         >
           <a
@@ -21,7 +21,7 @@
             v-for="size in toolsData.strokeWeight.slice().reverse()"
             @click.prevent="setStrokeWeight(size)"
           >
-            <svg height="40" width="60">
+            <svg height="40" width="40">
               <g>
                 <circle :cx="size" cy="20" :r="size" fill="black" />
               </g>
@@ -43,10 +43,7 @@
         >
       </div>
       <div class="tool-icon-container color-pick-tool">
-        <div
-          :class="[toolState.colorPickerMenu ? 'colors' : 'hidden']"
-          class="colors"
-        >
+        <div :class="['', toolState.colorPickerMenu ? 'enabled' : 'hidden']">
           <a
             href="#"
             @click.prevent="setColor(color)"
@@ -60,11 +57,34 @@
             </svg>
           </a>
         </div>
-        <img
-          @click="openColorMenu()"
-          class="tool-icon"
-          :src="require(`../assets/icons/colorfill.png`)"
-        />
+        <div class="">
+          <svg
+            @click="openColorMenu()"
+            :class="[
+              'tool-icon',
+              toolState.colorPickerMenu
+                ? 'background-enable'
+                : 'background-disable'
+            ]"
+            width="60"
+            height="60"
+            viewBox="0 0 18 34"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M22.9893 -0.988991C21.6413 -2.337 19.4553 -2.337 18.1073 -0.988991L13.5168 3.60153L12.9338 3.01861C12.6059 2.69072 12.0959 2.69072 11.768 3.01861L11.1851 3.60153C10.8572 3.92943 10.8572 4.43949 11.1851 4.76738L14.209 7.7913L11.768 5.35031L6.08447 11.0702C7.86968 11.2524 9.03552 10.9245 10.1649 10.5602C11.6951 10.0866 13.2617 9.61294 15.7756 10.7059C15.8484 10.7424 15.9213 10.8152 15.9942 10.8516L16.65 10.1959L17.2693 10.8152C17.5972 11.1431 18.1073 11.1431 18.4352 10.8152L19.0181 10.2323C19.346 9.9044 19.346 9.39434 19.0181 9.06644L18.4352 8.48352L22.9893 3.92943C24.3373 2.58142 24.3373 0.35902 22.9893 -0.988991Z"
+              :fill="[toolState.colorPickerMenu ? 'white' : 'black']"
+            />
+            <path
+              d="M14.9374 11.9087C13.0793 11.18 11.9863 11.5079 10.6019 11.9087C9.61819 12.2001 8.5252 12.528 7.06789 12.528C6.55784 12.528 5.97491 12.4916 5.31912 12.3823C5.17339 12.3459 5.02766 12.273 4.91836 12.1637L4.26257 12.8924L1.23865 19.851C1.02006 20.3611 1.53012 20.8347 2.00374 20.6525L9.10813 17.7379L14.9374 11.9087Z"
+              :fill="[toolState.colorPickerMenu ? 'white' : 'black']"
+            />
+            <path
+              d="M0 26.5173C0 27.6103 0.874386 28.5211 2.0038 28.5211C3.09678 28.5211 4.0076 27.6467 4.0076 26.5173C4.0076 25.4243 2.0038 22.874 2.0038 22.874C2.0038 22.874 0 25.4243 0 26.5173Z"
+              :fill="[toolState.colorPickerMenu ? 'white' : 'black']"
+            />
+          </svg>
+        </div>
       </div>
     </div>
   </div>
@@ -140,7 +160,7 @@ export default {
 .strokes {
   display: flex;
 }
-.colors {
+.enabled {
   display: flex;
 }
 
@@ -150,6 +170,17 @@ export default {
 
 .tool-icon-container {
   display: flex;
+}
+
+.background-disable {
+  background-color: #f0f0f0;
+  border: 0px solid #f0f0f0;
+}
+
+.background-enable {
+  background-color: #000;
+  border: 0px solid #000;
+  mix-blend-mode: difference;
 }
 
 .tool-list {
@@ -165,11 +196,9 @@ export default {
     max-width: 100%;
     height: auto;
     text-align: center;
-    border: 0px solid #f0f0f0;
     border-radius: 100%;
     position: relative;
     border-radius: 50%;
-    background-color: #f0f0f0;
   }
   .stroke-size-tool {
     display: flex;
@@ -182,6 +211,7 @@ export default {
     margin: 0;
     padding: 0;
     background-color: #f0f0f0;
+    mix-blend-mode: difference;
   }
   .tool-icon:hover {
     transition: 0.1s;
