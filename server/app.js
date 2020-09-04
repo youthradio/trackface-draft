1604e18d17314ca71dbe615302457e33a40a3b9d
Radames Ajna <radamajna@gmail.com>
2020-09-03 23:49:58 -0700

    move to yarn

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
-require("dotenv").config();
-const express = require("express");
-const multer = require("multer");
-const mime = require("mime");
+require('dotenv').config();
+const express = require('express');
+const multer = require('multer');
+const mime = require('mime');
 const memStorage = multer.memoryStorage();
-const AWSFaceRecognition = require("./awsFaceRekognition");
-const cors = require("cors");
+const AWSFaceRecognition = require('./awsFaceRekognition');
+const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware');
+const cors = require('cors');
 
 // var storage = multer.diskStorage({
 //   destination: function(req, file, cb) {
@@ -15,57 +16,61 @@ const cors = require("cors");
 //     cb(null, `${file.fieldname}-${uniqueSuffix}.${mime.getExtension(file.mimetype)}`);
 //   }
 // });
+
 const corsOptions = {
-  origin: "*",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  origin: ['https://youthradio.github.io', 'http://localhost:3000'],
+  methods: 'OPTIONS,GET,HEAD,PUT,PATCH,POST,DELETE',
   preflightContinue: false,
-  optionsSuccessStatus: 204
+  optionsSuccessStatus: 204,
+  credentials: false,
+  allowedHeaders: 'Content-Type',
 };
 const upload = multer({
   storage: memStorage,
   limits: {
     fields: 2,
-    fileSize: "5MB",
+    fileSize: '15MB',
     files: 2,
-    parts: 4
   },
-  fileFilter: function(req, file, cb) {
+  fileFilter: function (req, file, callback) {
     const filetypes = /jpeg|jpg|png/;
     const mimetype = mime.getExtension(file.mimetype);
 
     if (filetypes.test(mimetype)) {
-      return cb(null, true);
+      return callback(null, true);
     }
-    cb(
+    callback(
       new Error(
-        "File upload only supports the following filetypes - " + filetypes
+        'File upload only supports the following filetypes - ' + filetypes
       ),
       false
     );
-  }
+  },
 }).fields([
-  { name: "referenceimage", maxCount: 1 },
-  { name: "targetimage", maxCount: 1 }
+  { name: 'referenceimage', maxCount: 1 },
+  { name: 'targetimage', maxCount: 1 },
 ]);
 
 const app = express();
 app.use(cors(corsOptions));
-
+app.use(awsServerlessExpressMiddleware.eventContext());
 const faceReg = new AWSFaceRecognition();
 
-app.get("/", (req, res) => {
-  res.send("Nothing here");
+app.get('/', (req, res) => {
+  console.log('ROOT access NOTHING HERE');
+  res.send('Nothing here');
 });
 
-app.post("/push", async (req, res) =>
+app.post('/push', async (req, res) =>
   new Promise((resolve, reject) => {
     try {
-      upload(req, res, async err => {
+      upload(req, res, async (err) => {
         if (err) {
+          console.log('Error: Multer Upload', JSON.stringify(err, null, 2));
           return res.status(500).send({ error: err.stack });
         }
-        if (!("referenceimage" in req.files) || !("targetimage" in req.files)) {
-          const err = new Error("Error: Need two input files");
+        if (!('referenceimage' in req.files) || !('targetimage' in req.files)) {
+          const err = new Error('Error: Need two input files');
           res.status(500).send({ error: err.stack });
           return reject(err);
         }
@@ -74,15 +79,19 @@ app.post("/push", async (req, res) =>
             req.files.referenceimage[0].buffer,
             req.files.targetimage[0].buffer
           );
-          resolve(Object.assign(results, { error: "success" }));
+          console.log('Sucess: Results');
+          resolve(Object.assign(results, { error: 'success' }));
         } catch (err) {
+          console.log('Error: faceReg process', JSON.stringify(err, null, 2));
           resolve(Object.assign({}, { error: err }));
         }
       });
     } catch (err) {
+      console.log('Error: Upload catch', JSON.stringify(err, null, 2));
+      return res.status(500).send({ error: err.stack });
       reject(err);
     }
-  }).then(results => {
+  }).then((results) => {
     res.json(results);
   })
 );
--- server/lambda.js
+++ server/lambda.js
@@ -1,15 +1,18 @@
-"use strict";
-const awsServerlessExpress = require("aws-serverless-express");
-const app = require("./app");
+'use strict';
+const awsServerlessExpress = require('aws-serverless-express');
+const app = require('./app');
 const binaryMimeTypes = [
-  "application/octet-stream",
-  "font/eot",
-  "font/opentype",
-  "font/otf",
-  "image/jpeg",
-  "image/png",
-  "image/svg+xml"
+  'application/octet-stream',
+  'font/eot',
+  'font/opentype',
+  'font/otf',
+  'image/jpeg',
+  'image/png',
+  'image/svg+xml',
 ];
 const server = awsServerlessExpress.createServer(app, null, binaryMimeTypes);
-exports.handler = (event, context) =>
-  awsServerlessExpress.proxy(server, event, context);
+exports.handler = (event, context) => {
+  console.log('ENVIRONMENT VARIABLES\n' + JSON.stringify(process.env, null, 2));
+  console.warn('Event not processed.');
+  return awsServerlessExpress.proxy(server, event, context);
+};
